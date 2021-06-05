502c72195ce28e970e306f71aaa727af09d830e7
kathu08 <79391323+kathu08@users.noreply.github.com>
2021-06-05 17:09:04 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Industrial Trainings and Internships</h1>
+      <p>
+        Learn Artificial Intelligence and Full-Stack Web Development from people
+        at Google, Goldman Sachs, American Express, Jio and Microsoft.
+        World-Class Teachers, Large Student Community, Top Notch LIVE Courses
+        and One-to-One Mentorship are the features available in ShapeAI.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
